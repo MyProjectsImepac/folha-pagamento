0a4359fc139eb8f9ac382cb987d872d18a7eecef
Fuullero <galexandre434@gmail.com>
2023-11-27 20:49:37 -0300

    Commit

--- src/main/java/br/edu/imepac/folhapagamento/domains/teacher/TeacherDTO.java
+++ src/main/java/br/edu/imepac/folhapagamento/domains/teacher/TeacherDTO.java
@@ -5,6 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
