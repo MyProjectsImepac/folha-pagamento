e5b634c86b75b8a1b9febe644eac902c549d06fb
Guilherme Nasciutti <guilhermenasciutti@hotmail.com>
2023-11-28 20:39:42 -0300

    Desenvolvimento das classes da aplicação

---
+++ src/main/java/br/edu/imepac/folhapagamento/controllers/CollaboratorController.java
@@ -0,0 +1,52 @@
+package br.edu.imepac.folhapagamento.controllers;
+
+import br.edu.imepac.folhapagamento.domains.teacher.CollaboratorCreateRequest;
+import br.edu.imepac.folhapagamento.domains.teacher.CollaboratorDTO;
+import br.edu.imepac.folhapagamento.domains.teacher.TeacherCreateRequest;
+import br.edu.imepac.folhapagamento.domains.teacher.TeacherDTO;
+import br.edu.imepac.folhapagamento.services.CollaboratorService;
+import br.edu.imepac.folhapagamento.services.TeacherService;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/collaborator")
+public class CollaboratorController {
+
+    private final CollaboratorService collaboratorService;
+
+    public CollaboratorController(CollaboratorService collaboratorService) {
+        this.collaboratorService = collaboratorService;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.POST)
+    public CollaboratorDTO addCollaborator(@RequestBody @Valid CollaboratorCreateRequest collaboratorCreateRequest) {
+        return collaboratorService.save(collaboratorCreateRequest);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.GET)
+    public List<CollaboratorDTO> getAllCollaborator() {
+        return collaboratorService.findAll();
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public CollaboratorDTO getCollaboratorById(@PathVariable("id") Long id) {
+        return collaboratorService.findById(id);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deleteCollaborator(@PathVariable("id") Long id) {
+        collaboratorService.delete(id);
+    }
+}
--- src/main/java/br/edu/imepac/folhapagamento/entities/Address.java
+++ src/main/java/br/edu/imepac/folhapagamento/entities/Address.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
+import javax.validation.constraints.NotBlank;
 
 @Data
 @AllArgsConstructor
